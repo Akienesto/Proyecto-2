49a5fd1128e2ccec35860351719dee2c4b770981
Arturo <akienesto@gmail.com>
2022-02-04 16:21:21 +0100

    cambios 04/02/22

--- index1.js
+++ index1.js
@@ -10,36 +10,55 @@ let partidos = (datos1.matches)
 
 function crearTabla(matches) {
     let tabla_body = document.getElementById("tabla_body");
+    tabla_body.innerText = ""
     for (let i = 0; i < matches.length; i++) {
         const tr = document.createElement("tr");
         let local1 = document.createElement("img");
         local1.setAttribute("src", "https://crests.football-data.org/" + matches[i].homeTeam.id + ".svg")
+        local1.classList.add("imagenes")
         let local = document.createElement("p");
         local.innerHTML = matches[i].homeTeam.name;
         let visitante1 = document.createElement("img");
         visitante1.setAttribute("src", "https://crests.football-data.org/" + matches[i].awayTeam.id + ".svg")
+        visitante1.classList.add("imagenes")
         let visitante = document.createElement("p");
         visitante.innerHTML = matches[i].awayTeam.name;
-        let resultado1 = document.createElement("p");
-        resultado1.innerHTML = matches[i].score.fullTime.homeTeam;
-        resultado1.classList.add("resultado1");
-        let resultado2 = document.createElement("p");
-        resultado2.innerHTML = matches[i].score.fullTime.awayTeam;
-        resultado2.classList.add("resultado2");
-        let fecha = document.createElement("p");
-        fecha.innerHTML = matches[i].utcDate;
+         resultado =  matches[i].score.fullTime.homeTeam + "-" + matches[i].score.fullTime.awayTeam
+        if (resultado === "null-null"){
+            resultado = "Por jugar"
+        }else {
+            resultado.textContent =  matches[i].score.fullTime.homeTeam + "-" + matches[i].score.fullTime.awayTeam
+        }
+        let fecha = new Date(matches[i].utcDate);
         let jornada = document.createElement("p");
         jornada.innerHTML = matches[i].matchday;
-        let datos3 = [local1,local, resultado1, resultado2, visitante,visitante1, fecha, jornada];
+        jornada.classList.add("jornada")
+        let datos3 = [local1, local, resultado, visitante, visitante1, fecha.toLocaleString(), jornada];
         for (let j = 0; j < datos3.length; j++) {
             const td = document.createElement("td");
             td.append(datos3[j]);
             tr.appendChild(td);
         }
         tabla_body.appendChild(tr);
-        local1.classList.add("imagenes")
-        visitante1.classList.add("imagenes")
     }
 }
-
 crearTabla(partidos);
+
+let boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+    filtros(partidos);
+})
+
+function filtros(equipos) {
+    let buscar = document.getElementById("buscarEq").value;
+    let inputEq = equipos.filter(e => {
+        if ((e.homeTeam.name.toLowerCase().includes(buscar.toLowerCase())) || (e.awayTeam.name.toLowerCase().includes(buscar.toLowerCase()))){
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+    crearTabla(inputEq);
+}
+
